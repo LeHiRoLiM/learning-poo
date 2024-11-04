96671263b6a52ecf3dddc15ca536009a04cd7e53
Lehi - ASUS Vivo <lehirolim@gmail.com>
2024-11-04 17:02:57 -0300

    Atualizando

--- src/test/learning/Methods/Caneta.java
+++ src/test/learning/Methods/Caneta.java
@@ -2,29 +2,38 @@ package Methods;
 
 public class Caneta
 {
-    public String modelo;
+    private String modelo;
     private float ponta;
+    private boolean tampada;
+    private String cor;
 
-    public String getModelo()
-    {
+    public Caneta() {
+        this.tampar();
+        this.cor = "blue";
+    }
+    public String getModelo() {
         return this.modelo;
     }
-    public void setModelo(String m)
-    {
+    public void setModelo(String m) {
         this.modelo = m;
     }
-    public float getPonta()
-    {
+    public float getPonta() {
         return this.ponta;
     }
-    public void setPonta(float p)
-    {
+    public void setPonta(float p) {
         this.ponta = p;
     }
-    public void status ()
-    {
+    public void tampar(){
+        this.tampada = true;
+    }
+    public void destampar(){
+        this.tampada = false;
+    }
+    public void status () {
         System.out.println("Sobre a Caneta: ");
-        System.out.println("Modelo :" + this.modelo);
-        System.out.println("Ponta: "+ this.ponta);
+        System.out.println("Modelo: " + this.getModelo());
+        System.out.println("Ponta: "+ this.getPonta());
+        System.out.println("Cor "+ this.cor);
+        System.out.println("Tampada "+ this.tampada );
     }
 }
--- src/test/learning/Methods/Show.java
+++ src/test/learning/Methods/Show.java
@@ -5,9 +5,8 @@ public class Show
     public static void main(String[] args)
     {
         Caneta c1 = new Caneta();
-        c1.setModelo("BIC");
-        c1.setPonta(0.8f);
         c1.status();
+
 
     }
 }
