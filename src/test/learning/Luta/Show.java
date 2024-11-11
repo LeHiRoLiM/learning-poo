f41f429470e9cb3a5ee2fca655e442c366a380b4
Lehi - ASUS Vivo <lehirolim@gmail.com>
2024-11-11 19:02:55 -0300

    Atualizando

--- src/test/learning/Luta/Show.java
+++ src/test/learning/Luta/Show.java
@@ -7,9 +7,9 @@ public class Show
         Lutador[] lutadors = new Lutador[2];
         lutadors[0] = new Lutador("Lehi", "Brasil", 23, 1.56f,59,2,4,2);
         lutadors[1] = new Lutador("Gabriel","Argentina",20,1.70f,62.6f,1,5,3);
-        lutadors[1].apresentar();
-        lutadors[1].status();
 
-
+        Luta UEC01 = new Luta();
+        UEC01.marcarLuta(lutadors [0], lutadors [1]);
+        UEC01.lutar();
     }
 }
