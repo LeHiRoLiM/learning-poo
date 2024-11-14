6b233284a440309cae8500e2c6d642194e0f4af2
Lehi - ASUS Vivo <lehirolim@gmail.com>
2024-11-14 17:39:05 -0300

    Nova tarefa

---
+++ src/test/learning/Polimorfismo/Peixe.java
@@ -0,0 +1,35 @@
+package Polimorfismo;
+
+public class Peixe extends Animal
+{
+    private String corEscama;
+
+    @Override
+    public void locomover() {
+        System.out.println("Nadando");
+    }
+
+    @Override
+    public void alimentar() {
+        System.out.println("Comendo Substâncias");
+    }
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Soltar uma bolha");
+    }
+
+    public void soltarBolha() {
+        System.out.println("Soltando bolha");
+    }
+
+    public String getCorEscama()
+    {
+        return corEscama;
+    }
+
+    public void setCorEscama(String corEscama)
+    {
+        this.corEscama = corEscama;
+    }
+}
